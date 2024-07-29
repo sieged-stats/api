61263cc1d707a4413cf92ef8cc15d5724f93e07e
moyonenzo <moyon_e@etna-alternance.net>
2024-07-29 17:14:52 +0200

    add operators icons assets

--- src/app.py
+++ src/app.py
@@ -4,6 +4,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from src.routes import router as index_router
 from src.routes.search import router as search_router
 from src.routes.players import router as player_router
+from src.routes.assets import router as asset_router
 
 app = FastAPI(title="Sieged", version="0.1")
 
@@ -18,3 +19,4 @@ app.add_middleware(
 app.include_router(index_router, tags=["Index"])
 app.include_router(player_router, prefix="/players", tags=["Players"])
 app.include_router(search_router, prefix="/search", tags=["Search"])
+app.include_router(asset_router, prefix="/assets", tags=["Assets"])
